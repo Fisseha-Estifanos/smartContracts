daec838d81accc8c3bfda5ece4f2060b2ffc2462
fish <fisseha.137@gmail.com>
2022-07-28 12:56:40 +0300

    smartcontract: my inheritance and factory smart contract

---
+++ LastContract.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT;
+
+/*
+In this lesson we will learn
+
+Inheritance:
+
+Factories:
+
+Interaction with other smart contracts: 
+*/
+
+contract LastContract {
+
+
+}
